3aa4f43f51e95b717ede9a216a0ac4a1c084be2c
matheuskarnas <matheuskarnas1@gmail.com>
2021-11-26 02:27:39 -0300

    Commit do projeto funcional

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Header } from './components/Header/index';
+import { GlobalStyle } from './globalStyle'
+import { Hero } from './components/Hero';
+import { Short } from './components/Short';
+import { Text } from './components/Text/index'
+import { Rodape } from './components/Rodape';
+import { BoxInfoGrup } from './components/BoxInfoGrup/index';
+import { LastFunction } from './components/LastFunction/index';
+import { Top, Medium, Container, Botton } from './components/styled';
+import { Gambiarra } from './components/Gambiarra';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Top>
+        <Header />
+        <Hero />        
+      </Top>
+
+      <Medium>
+        <Short />
+        <Text />
+        <BoxInfoGrup />
+        <Gambiarra />
+      </Medium>
+
+      <Botton>
+        <LastFunction />
+        <Rodape />
+      </Botton>
+      <GlobalStyle />
+    </Container>
   );
 }
 
